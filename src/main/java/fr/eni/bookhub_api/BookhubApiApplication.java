f586e1fb8a4b5a1e5e437d03ddff1159326ab84c
Vivien <vivien.bodet2025@campus-eni.fr>
2026-04-20 15:11:07 +0200

    Initial commit

---
+++ src/main/java/fr/eni/bookhub_api/BookhubApiApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.bookhub_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookhubApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookhubApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/eni/bookhub_api/BookhubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.bookhub_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookhubApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
